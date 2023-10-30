711026b048b1897b3df54f899506131170a5fa0b
sage1500 <motosage25@gmail.com>
2023-10-30 20:55:49 +0900

    他のパラメータもプロパティ化＆コメントアウト部分削除

--- rest-client/src/main/java/com/example/rest/client/RestClientConfig.java
+++ rest-client/src/main/java/com/example/rest/client/RestClientConfig.java
@@ -1,11 +1,12 @@
 package com.example.rest.client;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.core5.util.TimeValue;
-import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,67 +25,62 @@ public class RestClientConfig {
 
 	@Value("${demo.socket-timeout:15000}")
 	private int socketTimeout = 15000;
-	
-	
+
+	@Value("${demo.max-conn-per-route:20}")
+	private int maxConnPerRoute = 20;
+
+	@Value("${demo.max-conn-total:20}")
+	private int maxConnTotal = 20;
+
+	@Value("${demo.max-retries:1}")
+	private int maxRetries = 1;
+
+	@Value("${demo.retry-interval:1000}")
+	private int retryInterval = 1000;
+
+	@Value("${demo.base-url:http://localhost:8080}") // memo: 最後に "/" をつけない
+	//@Value("${demo.base-url:http://192.168.1.100:8080}") // ConnectionTimeout確認用
+	private String baseUrl = "http://localhost:8080";
+
 	@Bean
-	public PetsApi petApi() {
-		
+	public RestTemplate restTemplateForPetsApi() {
 		// ConnectionManager
-		// 【設定】
-		// - コネクションプーリング
-		// @formatter:off
-		var connectionManager = 	PoolingHttpClientConnectionManagerBuilder.create()
-			.setMaxConnPerRoute(20)
-			.setMaxConnTotal(20)
-			//.setDefaultSocketConfig(null)
-			.setDefaultConnectionConfig(ConnectionConfig.custom()
-					.setConnectTimeout(Timeout.ofMilliseconds(connectTimeout))
-					.setSocketTimeout(Timeout.ofMilliseconds(socketTimeout))
-					.build())
-			.build();
-		// @formatter:on
-		
-		// HttpClient
-		// 【設定】
-		// - 再送信
-		// - プロキシ
-		// @formatter:off
-		var httpClient = HttpClientBuilder.create()
-				.setConnectionManager(connectionManager)
-				.setConnectionManagerShared(true)
-				// .setBackoffManager(null)
-				// .setConnectionBackoffStrategy(null)
-				// .setConnectionReuseStrategy(null)
-				// .setRetryStrategy(null) // デフォルトは new DefaultHttpRequestRetryStrategy()
-				//.setRetryStrategy(new DefaultHttpRequestRetryStrategy(2, TimeValue.ofSeconds(1))) //
-				.setRetryStrategy(new DefaultHttpRequestRetryStrategy(0, TimeValue.ofSeconds(1))) //
-				// .setProxy(null)
+		var connectionManager = PoolingHttpClientConnectionManagerBuilder.create() //
+				.setDefaultConnectionConfig(ConnectionConfig.custom() //
+						.setConnectTimeout(connectTimeout, TimeUnit.MILLISECONDS) //
+						.setSocketTimeout(socketTimeout, TimeUnit.MILLISECONDS) //
+						.build()) //
+				.setMaxConnPerRoute(maxConnPerRoute) //
+				.setMaxConnTotal(maxConnTotal) //
 				.build();
-		// @formatter:on
+
+		// HttpClient
+		var httpClient = HttpClientBuilder.create() //
+				.setConnectionManager(connectionManager) //
+				.setConnectionManagerShared(true) //
+				.setRetryStrategy(
+						new DefaultHttpRequestRetryStrategy(maxRetries, TimeValue.ofMilliseconds(retryInterval))) //
+				.build();
 
 		// RequestFactory
-		// 【設定】
-		// - タイムアウト
 		var requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		requestFactory.setConnectionRequestTimeout(connectTimeout); // コネクションプールから取得するタイムアウト
-		// requestFactory.setConnectTimeout(connectTimeout);
+		requestFactory.setConnectionRequestTimeout(connectTimeout);
 
 		// RestTemplate
-        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
-        uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+		DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
+		uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 		var restTemplate = new RestTemplate();
 		restTemplate.setRequestFactory(requestFactory);
-        restTemplate.setUriTemplateHandler(uriBuilderFactory);
+		restTemplate.setUriTemplateHandler(uriBuilderFactory);
 
-		// ApiClient
-		// 【設定】
-		// - ベースパス
-		var apiClient = new ApiClient(restTemplate) //
-					.setBasePath("http://localhost:8080"); // memo: 最後に "/" をつけない
-					// .setBasePath("http://192.168.1.100:8080"); // ConnectionTimeout確認用
-		
-		// PetStore
+		return restTemplate;
+	}
+
+	@Bean
+	public PetsApi petApi(RestTemplate restTemplateForPetsApi) {
+		var apiClient = new ApiClient(restTemplateForPetsApi) //
+				.setBasePath(baseUrl);
 		return new PetsApi(apiClient);
 	}
-	
+
 }
